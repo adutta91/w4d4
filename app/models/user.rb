44c41954533cb7c07170616960c4e96289de7494
Arjun <arjundutta91@gmail.com>
2016-01-28 11:19:42 -0800

    initial commit, add user model and controller, session controller

---
+++ app/models/user.rb
@@ -0,0 +1,40 @@
+class User < ActiveRecord::Base
+
+  validates :email, :password_digest, :session_token, presence: true
+  validates :email, :session_token, uniqueness: true
+
+  attr_reader :password
+
+  after_initialize do |user|
+    user.ensure_session_token
+  end
+
+  def self.generate_session_token
+    SecureRandom.urlsafe_base64
+  end
+
+  def reset_session_token!
+    self[:session_token] = User.generate_session_token
+  end
+
+  def ensure_session_token
+    self[:session_token] ||= User.generate_session_token
+  end
+
+  def password=(password)
+    @password = password
+    self[:password_digest] = BCrypt::Password.create(password).to_s
+  end
+
+  def is_password?(password)
+    BCrypt::Password.new(self[:password_digest]).is_password?(password)
+  end
+
+  def find_by_credentials(email, password)
+    user = User.find_by(email: email)
+    return nil if user.nil?
+    return user if user.is_password?(password)
+    nil
+  end
+
+end
